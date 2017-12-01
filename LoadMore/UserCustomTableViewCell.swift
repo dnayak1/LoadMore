331172e613defa5094d9695d0d51845105448d83
Akshay <aayyanch@uncc.edu>
2017-11-30 19:56:24 -0500

    Custom Table view cells created and its classes created

---
+++ LoadMore/UserCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCustomTableViewCell.swift
+//  LoadMore
+//
+//  Created by Ayyanchira, Akshay Murari on 11/30/17.
+//  Copyright © 2017 group2. All rights reserved.
+//
+
+import UIKit
+
+class UserCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
